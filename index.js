6c0539155648e511465762b14f38aee823148859
uzairriaz <uzairriaz.pk@gmail.com>
2021-11-11 14:40:56 +0500

    R2D

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 mongoose
   .connect(DB)
   .then(() => {
