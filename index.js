18007c772603ef46bab3705a678d8cb642e25b28
uzairriaz <uzairriaz.pk@gmail.com>
2021-11-11 14:57:12 +0500

    R2D

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded());
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, DELETE");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
