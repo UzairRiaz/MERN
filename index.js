816b75523a4c5115b68dbe1052d5899815c77a99
uzairriaz <uzairriaz.pk@gmail.com>
2021-11-11 14:52:41 +0500

    R2D

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(express.urlencoded());
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
